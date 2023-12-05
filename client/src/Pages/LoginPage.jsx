3cf9e6b93452b0851988adffba1ba6918bd3c613
Anas Draiaf <anasaizen9@gmail.com>
2023-12-06 00:58:10 +0100

    FIX some errors

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -30,7 +30,7 @@ function LoginPage() {
             </div>
             
     <div className="mb-4 flex items-center">
-        <a href="#" class="text-main-400 hover:underline">Forgot password?</a>
+        <a href="#" className="text-main-400 hover:underline">Forgot password?</a>
     </div>
     <div className="flex items-center justify-between">
       <Button text="Login"></Button>
@@ -47,11 +47,11 @@ function LoginPage() {
   type="button"
   data-te-ripple-init
   data-te-ripple-color="light"
-  class="mb-2 inline-block bg-blue-700 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+  className="mb-2 inline-block bg-blue-700 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
   >
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-4 w-4"
+    className="h-4 w-4"
     fill="white"
     viewBox="0 0 24 24">
     <path
@@ -63,17 +63,17 @@ function LoginPage() {
   type="button"
   data-te-ripple-init
   data-te-ripple-color="light"
-  class="mb-2 inline-block bg-red-600 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+  className="mb-2 inline-block bg-red-600 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
 >
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-4 w-4"
+    className="h-4 w-4"
     fill="white"
     viewBox="0 0 24 24">
     <path
       d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
-      fill-rule="evenodd"
-      clip-rule="evenodd" />
+      fillRule="evenodd"
+      clipRule="evenodd" />
   </svg>
 </button>
  {/* linkdin */}
@@ -81,11 +81,11 @@ function LoginPage() {
   type="button"
   data-te-ripple-init
   data-te-ripple-color="light"
-  class="mb-2 inline-block bg-blue-500 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+  className="mb-2 inline-block bg-blue-500 rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
   >
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-4 w-4"
+    className="h-4 w-4"
     fill="white"
     viewBox="0 0 24 24">
     <path
@@ -94,7 +94,7 @@ function LoginPage() {
 </button>
 </div>
     </div>
-    <h2 className="ms-2 text-sm text-center ">Don't have an account ? <a href="#" class="text-main-400 hover:underline">Sign up</a></h2>
+    <h2 className="ms-2 text-sm text-center ">Don't have an account ? <a href="#" className="text-main-400 hover:underline">Sign up</a></h2>
 
   </form>
         </div>
