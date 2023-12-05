346a24eae93a98370c943d81124b171534c6f25f
zakaria <zakariaa@gmail.com>
2023-12-05 19:43:15 +0100

    mig

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('type',['client','admin','pharmacie']);
+            $table->enum('type',['client','pharmacie']);
             $table->rememberToken();
             $table->timestamps();
         });
