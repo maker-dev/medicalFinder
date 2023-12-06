09475525d1255a424f308642c5234a3bcc4d7541
Your Name <ekkomain93@gmail.com>
2023-12-06 17:14:34 +0100

    fix issue in card pharmacy

--- client/src/Components/cards/PharmacyCard.jsx
+++ client/src/Components/cards/PharmacyCard.jsx
@@ -9,7 +9,7 @@ export default function PharmacyCard(props){
                                         bg-white rounded-lg shadow-xl   ">
         
         <div className=" md:w-1/2 m-6 ">
-          <img className="inset-0 h-full w-full object-cover object-center" src={imgUrl === null ? aIcons.phaIcDefault : imgUrl} />
+          <img className="inset-0 h-full w-full object-cover object-center" src={imgUrl === null ? aIcons.phaIcDefault : imgUrl} alt="pharamcy"/>
         </div>
         
         <div className="w-full py-4 px-6 text-gray-800 flex flex-col justify-around  mt-2">
@@ -20,21 +20,6 @@ export default function PharmacyCard(props){
                 <p className="font-bold text-sm leading-tight truncate">Healthy Haven Pharmacy</p>
             </div>
 
-            <div className="w-full py-4 px-6 text-gray-800 flex flex-col justify-around  mt-2">
-              <div className="flex flex-col space-y-4 ">
-                <p className="font-bold text-lg leading-tight truncate">
-                  Healthy Haven Pharmacy
-                </p>
-                <p className="font-semibold text-sm text-slate-500 leading-tight truncate">
-                  Healthy Haven Pharmacy
-                </p>
-                <p className="font-semibold text-sm text-slate-500 leading-tight truncate">
-                  Healthy Haven Pharmacy
-                </p>
-                <p className="font-bold text-sm leading-tight truncate">
-                  Healthy Haven Pharmacy
-                </p>
-              </div>
 
               <p className="text-sm lg:mb-20 my-6">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
@@ -46,7 +31,7 @@ export default function PharmacyCard(props){
           </div>
         </div>
       </div>
-      </div>
+      
   
   );
 }
