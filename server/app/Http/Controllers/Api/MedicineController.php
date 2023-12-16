d346143e28c7c40404f57011920494f1a924d3fd
zakaria <zakariaa@gmail.com>
2023-12-16 18:09:27 +0100

    complet functions in MedicineController

--- server/app/Http/Controllers/Api/MedicineController.php
+++ server/app/Http/Controllers/Api/MedicineController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class MedicineController extends Controller
 {
@@ -21,6 +22,78 @@ class MedicineController extends Controller
     public function show(String $id) {
         $medicine = Medicine::find($id);
         return $this->success($medicine, null, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'category'=>'required',
+            'forAdult'=>'required'
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->storeAs('storage/images', $imageName); 
+    
+           
+            $request->merge(['image' => $imageName]);
+        }
+
+        $medicine = Medicine::create($request->all());
+
+       
+        return $this->success($medicine,null,200);
+    }
+
+    public function update(Request $request, $id)
+{
+    $medicine = Medicine::find($id);
+
+   
+
+    $request->validate([
+        'name' => 'required',
+        'description' => 'required',
+        'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'category'=>'required',
+        'forAdult'=>'required'
+    ]);
+
+    
+    if ($request->hasFile('image')) {
+        
+        if ($medicine->image) {
+            Storage::delete('storage/images/' . $medicine->image);
+        }
+
+        
+        $image = $request->file('image');
+        $imageName = time() . '.' . $image->getClientOriginalExtension();
+        $image->storeAs('storage/images', $imageName); 
+
+        
+        $request->merge(['image' => $imageName]);
+    }
+
+    $medicine->update($request->all());
+
+    return $this->success($medicine,null,200);
+}
+
+public function destroy($id)
+    {
+        $medicine = Medicine::find($id);
+
+       
+
+        $medicine->delete();
+
+        
+        return $this->success($medicine,null,200);
     }
 
     public function search(Request $request) {
