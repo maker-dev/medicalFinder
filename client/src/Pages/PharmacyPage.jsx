450d557fbbe373b8d6dfdcdaa6a4316529362d95
My name <ekkomain93@gmail.com>
2023-12-30 20:49:55 +0100

    PharmacyPage design finished

--- client/src/Pages/PharmacyPage.jsx
+++ client/src/Pages/PharmacyPage.jsx
@@ -107,6 +107,43 @@ export default function PharmacyPage() {
                                         <td className="py-6 px-4 border-r border-gray-300">
                                             <div className='flex md:flex-row flex-col items-center '>
                                             <div className="relative p-4 rounded-full border border-1 border-gray-400">
+                                                
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                <path d="M19.006 3.705a.75.75 0 1 0-.512-1.41L6 6.838V3a.75.75 0 0 0-.75-.75h-1.5A.75.75 0 0 0 3 3v4.93l-1.006.365a.75.75 0 0 0 .512 1.41l16.5-6Z" />
+                                                <path fillRule="evenodd" d="M3.019 11.114 18 5.667v3.421l4.006 1.457a.75.75 0 1 1-.512 1.41l-.494-.18v8.475h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3v-9.129l.019-.007ZM18 20.25v-9.566l1.5.546v9.02H18Zm-9-6a.75.75 0 0 0-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75H9Z" clipRule="evenodd" />
+                                            </svg>
+
+                                            </div>
+
+                                            <div className="mt-4 md:mt-0 md:ml-4">
+                                                <p className="text-gray-600 text-sm font-medium text-center md:text-left">Address</p>
+                                                <p className="text-gray-600 text-sm font-medium text-center md:text-left" >{pharmacy.address}</p>
+                                            </div>
+                                            </div>
+                                            
+                                        </td>
+                                        <td className="py-6 px-4 border-r border-gray-300">
+                                            <div className='flex md:flex-row flex-col items-center '>
+                                            <div className="relative p-4 rounded-full border border-1 border-gray-400">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                    <path filrule="evenodd" d="M8.161 2.58a1.875 1.875 0 0 1 1.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0 1 21.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 0 1-1.676 0l-4.994-2.497a.375.375 0 0 0-.336 0l-3.868 1.935A1.875 1.875 0 0 1 2.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437ZM9 6a.75.75 0 0 1 .75.75V15a.75.75 0 0 1-1.5 0V6.75A.75.75 0 0 1 9 6Zm6.75 3a.75.75 0 0 0-1.5 0v8.25a.75.75 0 0 0 1.5 0V9Z" clipRule="evenodd" />
+                                                </svg>
+                                            </div>
+
+                                            <div className="mt-4 md:mt-0 md:ml-4">
+                                                <p className="text-gray-600 text-sm text-center font-medium md:text-left">City</p>
+                                                <p className="text-gray-600 text-sm text-center font-medium md:text-left">{pharmacy.city}</p>
+                                            </div>
+                                            </div>
+                                            
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                <tr className="border-b border-gray-300">
+                                        <td className="py-6 px-4 border-r border-gray-300">
+                                            <div className='flex md:flex-row flex-col items-center '>
+                                            <div className="relative p-4 rounded-full border border-1 border-gray-400">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                     <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
                                                 </svg>
@@ -117,7 +154,6 @@ export default function PharmacyPage() {
                                                 <p className="text-gray-600 text-sm font-medium text-center md:text-left" ><a className='text-red-700 underline font-bold hover:text-green-700' href={pharmacy.location ? `https://www.google.com/maps?q=${pharmacy.location.latitude},${pharmacy.location.longitude}` : "#"} target='_blank'>Location link</a></p>
                                             </div>
                                             </div>
-                                            
                                         </td>
                                         <td className="py-6 px-4 border-r border-gray-300">
                                             <div className='flex md:flex-row flex-col items-center '>
