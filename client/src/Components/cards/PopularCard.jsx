aaf054b63cafc3fbca3276b94f12a1e332f1fa7f
Abdelmawla <67197201+AbdelmawlaFellani@users.noreply.github.com>
2024-01-01 17:20:11 +0100

    Added styling to HowItWorks File

--- client/src/Components/cards/PopularCard.jsx
+++ client/src/Components/cards/PopularCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function PopularCard({ text, button }) {
   return (
-    <div className="flex h-128 bg-main-400 flex-col gap-7 max-w-sm rounded-md overflow-hidden shadow-lg p-10 justify-center items-center">
+    <div className="bg flex h-128 bg-main-400 flex-col gap-7 max-w-sm rounded-md overflow-hidden shadow-lg p-10 justify-center items-center">
       <div className="text-5xl text-center">{text}</div>
       <button className=" text-center px-5 py-2 rounded-md bg-secondary text-white font-mina_bold">
         {button}
